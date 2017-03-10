3cfc329be3c19d64fe2d8031956400a799ba4b4b
congge <congge@exapmp.com>
2017-03-10 21:11:29 +0800

    newlist

---
+++ php/http.php
@@ -0,0 +1,7 @@
+<?php
+$type = $_GET['type'];
+$callback = $_GET['callback'];
+$url = "http://v.juhe.cn/toutiao/index?type=".$_GET['type']."&key=506215dbc2302fb8b6a8aa5b4cb605eb";
+$ret = file_get_contents($url); 
+echo $callback."(".json_encode($ret).")";
+?>
